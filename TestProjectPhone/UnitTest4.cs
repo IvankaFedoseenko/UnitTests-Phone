152a37fa0eb4fba88a9bd18893c6eecd8f0264ae
Ivanna Fedoseenko <ivanna.fedoseenko@microsoft.wsei.edu.pl>
2025-03-20 20:31:16 +0100

    phone

--- TestProjectPhone/UnitTest4.cs
+++ TestProjectPhone/UnitTest4.cs
@@ -15,5 +15,20 @@ namespace TestProjectPhone
             Assert.AreEqual(1, phone.Count);
             Assert.IsFalse(phone.AddContact("Kaczynski", "987654322"));
         }
+        [TestMethod]
+        public void Test_Add_Contact_Limit()
+        {
+            var phone = new Phone("Molenda", "123456789");
+
+            for (int i = 0; i < 101; i++)
+            {
+                if (phone.Count >= phone.PhoneBookCapacity)
+                {
+                    Assert.ThrowsException<InvalidOperationException>(() => phone.AddContact($"Kowalski{i}", "000000000"));
+                    break;
+                }
+                phone.AddContact($"Kowalski{i}", $"000000000");
+            }
+        }
     }
 }
---
+++ TestProjectPhone/UnitTest5.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+namespace TestProjectPhone
+{
+    [TestClass]
+    public class PhoneCall
+    {
+        [TestMethod]
+        public void Test_Phone_Call()
+        {
+            var phone = new Phone("Molenda", "123456789");
+            string name = "Molenda 2";
+            string number = "987654321";
+            phone.AddContact(name, number);
+            Assert.ThrowsException<InvalidOperationException>(() => phone.Call("Molenda 3"));
+            Assert.AreEqual(phone.Call("Molenda 2"), $"Calling {number} ({name}) ...");
+        }
+    }
+}
